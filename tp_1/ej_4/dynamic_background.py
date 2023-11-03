91d23242f69fa2945f367847b369cfb516878e1a
Javier Cremona <cremona@cifasis-conicet.gov.ar>
2023-11-03 15:22:18 -0300

    WIP

--- tp_1/ej_4/dynamic_background.py
+++ tp_1/ej_4/dynamic_background.py
@@ -22,33 +22,42 @@ if __name__ == "__main__":
 
         h,w,c = frame.shape
         bg = background[:h,:w,:]
-        green_channel = frame[:,:,1]
-        mask_bool = (green_channel != 255)
-        mask = np.uint8(mask_bool)
-        mask *= 255        
+        # green_channel = frame[:,:,1]
+        # mask_bool = (green_channel != 255)
+        # mask = np.uint8(mask_bool)
+        # mask *= 255        
+        green_blue_ratio = (frame[:,:,1] / (frame[:,:,0]+0.1)).astype('uint8')
+        green_red_ratio = (frame[:,:,1] / (frame[:,:,2]+0.1)).astype('uint8')
+        # FIXME so many values are near 1 because 255 divided by a similar number is near 1 
+        # Could be solved applying a log to the divisor? or something similar
+        _,thresh1 = cv.threshold(green_blue_ratio,1,255,cv.THRESH_BINARY_INV)
+        _,thresh2 = cv.threshold(green_red_ratio,1,255,cv.THRESH_BINARY_INV)
+        # FIXME perhaps it could be improved using the former mask ((green_channel != 255))
+        mask = cv.add(thresh1,thresh2)
+        #mask = cv.subtract(mask, cv.bitwise_not(cm))        
         mask_inv = cv.bitwise_not(mask)
         background_img = cv.bitwise_and(bg,bg,mask=mask_inv)
         person = cv.bitwise_and(frame,frame,mask=mask)
 
         # Blur the walking man
-        blurred_person = cv.GaussianBlur(person, (61, 61), 0)
+        blurred_person = cv.GaussianBlur(person, (3, 3), 0)
         #sobel = cv.Sobel(src=mask, ddepth=cv.CV_8U, dx=1, dy=1, ksize=1)
         #sobel = np.stack((sobel,)*3, axis=-1)
         #canny = cv.Canny(image=mask, threshold1=0, threshold2=10)
         #canny = np.stack((canny,)*3, axis=-1)
         
         # Find the contour
-        contours, hierarchy = cv.findContours(mask.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)        
+        contours, hierarchy = cv.findContours(mask.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
         # Obtain a contour mask
         contour_mask = np.zeros(frame.shape, np.uint8)
         cv.drawContours(contour_mask, contours, -1, (255,255,255),5)        
-        
+
         # Replace the contour of the person with the blurred contour (using the contour mask)
         output = np.where(contour_mask==np.array([255, 255, 255]), blurred_person, person)
-        
+
         result_image = cv.add(output, background_img)
-        cv.imshow('frame', result_image)
+        cv.imshow('frame',result_image)     
 
         if cv.waitKey(10) == ord('q'):
             break   
